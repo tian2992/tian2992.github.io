8eeb76561740316e34dc000d58a667d9d688a7af
Sebastian Oliva <code@sebastianoliva.com>
2019-05-27 11:44:42 -0600

    import wp content

---
+++ wp-content/themes/tian/css/mainPag.js
@@ -0,0 +1,6 @@
+$(function() {
+	$("#postTitles").scrollable({clickable:false}).circular(); 
+	$("#twitTitles").scrollable({clickable:false}).circular();
+	$("#flickPotos").scrollable({clickable:false}).circular();
+	$("#lastSongos").scrollable({clickable:false}).circular();
+});
